NationalSecurityAgency/lemongraph

commit e41bd54365a960d69c3f0d6a6a297d89240aa646

split server.py into server/__init__.py and server/__main__.py to fix logging, fix exec updates for stock CentOS 7 python 2.7.5

LemonGraph/server/__init__.py
@@ -1,13 +1,12 @@
 from __future__ import print_function
-from . import Serializer, Node, Edge, Adapters, QuerySyntaxError, merge_values
-from .collection import Collection, uuid_to_utc
-from .lock import Lock
+from .. import Serializer, Node, Edge, QuerySyntaxError, merge_values
+from ..collection import Collection, uuid_to_utc
+from ..lock import Lock
 
 import atexit
 from collections import deque, defaultdict, namedtuple
 import dateutil.parser
 import errno
-from getopt import GetoptError, gnu_getopt as getopt
 import itertools
 from lazy import lazy
 import logging
@@ -24,7 +23,7 @@ from uuid import uuid1 as uuidgen
 
 from urlparse import parse_qs
 
-from .httpd import HTTPMethods, HTTPError, httpd, Graceful, json_encode, json_decode
+from ..httpd import HTTPMethods, HTTPError, httpd, Graceful, json_encode, json_decode
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -38,15 +37,6 @@ STRING = re.compile(r'^.+$')
 def date_to_timestamp(s):
     dt = dateutil.parser.parse(s)
     return time.mktime(dt.utctimetuple()) + dt.microsecond / 1e6
-
-class LogHandler(logging.StreamHandler):
-    def __init__(self, stream=sys.stdout):
-        logging.StreamHandler.__init__(self, stream)
-        fmt = '%(asctime)-8s %(name)s(%(process)d).%(levelname)s: %(message)s'
-        fmt_date = '%Y-%m-%d %T %Z'
-        formatter = logging.Formatter(fmt, fmt_date)
-        self.setFormatter(formatter)
-
 
 def js_dumps(obj, pretty=False):
     txt = json_encode(obj)
@@ -715,8 +705,9 @@ class Reset_UUID(_Input, Handler):
                     pass
                 os.rename(path, target)
                 cleanup.pop() # unlink(path)
-                # bypass creds check, allow hooks to run
+                # remove from index
                 self.collection.remove(uuid)
+                # open to re-index, bypass creds check, allow hooks to run
 #                with self.collection.graph(uuid, readonly=True):
                 with self.collection.graph(uuid):
                     pass
@@ -796,6 +787,11 @@ class D3_UUID(_Streamy, Handler):
             pass
         yield ']}\n'
 
+def exec_wrapper(code, **gvars):
+    lvars = {}
+    exec(code, gvars, lvars)
+    return lvars
+
 class Graph_Exec(_Input, _Streamy):
     path = ('graph', 'exec')
     eat_params = ('enabled', 'user', 'role', 'created_after', 'created_before', 'filter')
@@ -804,8 +800,7 @@ class Graph_Exec(_Input, _Streamy):
         if self.content_type != 'application/python':
             raise HTTPError(409, 'Bad/missing Content-Type - must be: application/python')
         code = ''.join(self.body)
-        locals = {}
-        globals = {
+        gvars = {
             'dumps': self.dumps,
             'stream': self.stream,
             'HTTPError': HTTPError,
@@ -815,10 +810,10 @@ class Graph_Exec(_Input, _Streamy):
 
         txns_uuids = self._txns_uuids(uuids)
         try:
-            exec(code, globals, locals)
+            lvars = exec_wrapper(code, **gvars)
             params = dict((k, v) if len(v) > 1 else (k, v[0]) for k, v in self.params.iteritems() if k not in self.eat_params)
             try:
-                handler = locals['handler']
+                handler = lvars['handler']
             except KeyError:
                 raise HTTPError(404, 'no handler function defined - should look like: def handler(txns_uuids, headers, **kwargs)')
             return handler(txns_uuids, self.res.headers, **params)
@@ -838,23 +833,22 @@ class Graph_Exec(_Input, _Streamy):
                 pass
 
 class Graph_UUID_Exec(_Input, _Streamy):
-    path = ('graph', UUID,'exec')
+    path = ('graph', UUID, 'exec')
     @graphtxn(write=False)
     def post(self, g, txn, _, uuid, __):
         if self.content_type != 'application/python':
             raise HTTPError(409, 'Bad/missing Content-Type - must be: application/python')
         code = ''.join(self.body)
-        locals = {}
-        globals = {
+        gvars = {
             'dumps': self.dumps,
             'stream': self.stream,
             'HTTPError': HTTPError,
         }
         try:
-            exec(code, globals, locals)
+            lvars = exec_wrapper(code, **gvars)
             params = dict((k, v) if len(v) > 1 else (k, v[0]) for k, v in self.params.iteritems())
             try:
-                handler = locals['handler']
+                handler = lvars['handler']
             except KeyError:
                 raise HTTPError(404, 'no handler function defined - should look like: def handler(txn, uuid, headers, **kwargs)')
             return handler(txn, uuid, self.res.headers, **params)
@@ -1050,183 +1044,13 @@ class Server(object):
             Favicon,
             Static,
         )
+
+        global lock
+        lock = Lock('%s.lock' % collection_path)
+
+        global collection
+        collection = None
+
         handlers = tuple( H(collection_path=collection_path, graph_opts=graph_opts) for H in classes)
         kwargs['handlers'] = handlers
         httpd(**kwargs)
-
-def seed_depth0():
-    while True:
-        txn, entry = yield
-        # first checks stay w/in the entry - last has to instantiate the parent object
-        if entry.is_property and entry.key == 'seed' and entry.value and entry.is_node_property:
-            entry.parent['depth'] = 0
-
-def process_edge(txn, e, cost=1, inf=float('Inf')):
-    # grab latest versions of endpoints
-    src = txn.node(ID=e.srcID)
-    tgt = txn.node(ID=e.tgtID)
-    ds = src.get('depth', inf)
-    dt = tgt.get('depth', inf)
-    if ds + cost < dt:
-        tgt['depth'] = ds + cost
-    elif dt + cost < ds:
-        src['depth'] = dt + cost
-
-def apply_cost(txn, prop):
-    # cost value validity has already been enforced above
-    process_edge(txn, prop.parent, cost=prop.value)
-
-def cascade_depth(txn, prop):
-    # grab latest version of parent node
-    node = txn.node(ID=prop.parentID)
-    mindepth = prop.value + 1
-    for n2 in node.neighbors:
-        try:
-            if n2['depth'] <= mindepth:
-                continue
-        except KeyError:
-            pass
-        n2['depth'] = mindepth
-
-def update_depth_cost():
-    while True:
-        txn, entry = yield
-        if entry.is_edge:
-            # grab newly added edges, and propagate depth
-            process_edge(txn, entry)
-        elif entry.is_property:
-            if entry.key == 'depth':
-                if entry.is_node_property:
-                    cascade_depth(txn, entry)
-            elif entry.key == 'cost':
-                if entry.is_edge_property:
-                    apply_cost(txn, entry)
-
-def usage(msg=None, fh=sys.stderr):
-    print('Usage: python -mLemonGraph.server <options> [graphs-dir]', file=fh)
-    print('', file=fh)
-    print('Options:', file=fh)
-    print('  -i <bind-ip>        (127.0.0.1)', file=fh)
-    print('  -p <bind-port>      (8000)', file=fh)
-    print('  -d <poll-delay-ms>  (250)', file=fh)
-    print('  -w <workers>        (#cpu cores)', file=fh)
-    print('  -l <log-level>      (info)', file=fh)
-    print('  -t <timeout>        (3 seconds)', file=fh)
-    print('  -b <buflen>         (1048576)', file=fh)
-    print('  -s                  enable nosync for graph dbs', file=fh)
-    print('  -m                  enable nometasync for graph dbs', file=fh)
-    print('  -r                  rebuild index', file=fh)
-    print('  -h                  print this help and exit', file=fh)
-    if msg is not None:
-        print('', file=fh)
-        print(msg, file=fh)
-    sys.exit(1)
-
-def main():
-    levels = ('NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
-    try:
-        opts, args = getopt(sys.argv[1:], 'i:p:d:l:w:t:b:smrh')
-    except GetoptError as err:
-        usage(msg=str(err))
-    if len(args) > 1:
-        usage()
-
-    default_level = 'info'
-
-    ip = '127.0.0.1'
-    port = 8000
-    logspec = default_level
-    poll = 250
-    nosync = False
-    nometasync = False
-    rebuild = False
-    path = 'graphs'
-    workers = -1
-    timeout = 3
-    buflen = 1048576
-    try:
-        path = args[0]
-    except IndexError:
-        pass
-
-    try:
-        for o, a in opts:
-            if o == '-i':
-                ip = a
-            elif o == '-p':
-                port = sorted((0, int(a), 65536))[1]
-            elif o == '-d':
-                poll = sorted((20, int(a), 10000))[1]
-            elif o == '-s':
-                nosync = True
-            elif o == '-m':
-                nometasync = True
-            elif o == '-r':
-                rebuild = True
-            elif o == '-l':
-                logspec = a
-            elif o == '-t':
-                timeout = sorted((0.010, int(a)))[1]
-            elif o == '-w':
-                workers = int(a)
-            elif o == '-b':
-                buflen = int(a)
-            elif o == '-h':
-                usage(fh=sys.stdout)
-            else:
-                raise RuntimeError(o)
-    except (KeyError, ValueError):
-        usage()
-
-    all_logs = tuple("LemonGraph.%s" % x for x in ('httpd', 'collection', 'server'))
-    log_levels = dict( (k, default_level) for k in all_logs)
-    for token in logspec.split(','):
-        try:
-            target, level = token.split('=', 1)
-            alt = "LemonGraph.%s" % target
-            target = alt if alt in all_logs else target
-            targets = (target,)
-        except ValueError:
-            targets = all_logs
-            level = token
-        if level.upper() not in levels:
-            usage()
-        for target in targets:
-            log_levels[target] = level
-
-    loghandler = LogHandler()
-    for target, level in log_levels.iteritems():
-        logger = logging.getLogger(target)
-        logger.addHandler(loghandler)
-        logger.setLevel(getattr(logging, level.upper()))
-
-    graph_opts=dict(
-        serialize_property_value=Serializer.msgpack(),
-        adapters=Adapters(seed_depth0, update_depth_cost),
-        nosync=nosync, nometasync=nometasync,
-        )
-
-    # initialize the collection up front (which will re-create if missing)
-    # before we turn on the web server
-    col = Collection(path, create=True, rebuild=rebuild, graph_opts=graph_opts)
-    col.close()
-
-    def _syncd():
-        collection = Collection(path, graph_opts=graph_opts, nosync=True, nometasync=False)
-        try:
-            collection.daemon(poll=poll)
-        except Graceful:
-            pass
-        finally:
-            collection.close()
-
-    global collection
-    collection = None
-
-    global lock
-    lock = Lock('%s.lock' % path)
-
-    Server(collection_path=path, graph_opts=graph_opts, extra_procs={'syncd': _syncd}, host=ip, port=port, spawn=workers, timeout=timeout, buflen=buflen)
-
-if '__main__' == __name__:
-    main()

LemonGraph/server/__main__.py
@@ -1,0 +1,186 @@
+from __future__ import print_function
+from .. import Serializer, Adapters
+from ..collection import Collection
+from . import Server
+
+import sys
+import logging
+from getopt import GetoptError, gnu_getopt as getopt
+
+class LogHandler(logging.StreamHandler):
+    def __init__(self, stream=sys.stdout):
+        logging.StreamHandler.__init__(self, stream)
+        fmt = '%(asctime)-8s %(name)s(%(process)d).%(levelname)s: %(message)s'
+        fmt_date = '%Y-%m-%d %T %Z'
+        formatter = logging.Formatter(fmt, fmt_date)
+        self.setFormatter(formatter)
+
+def usage(msg=None, fh=sys.stderr):
+    print('Usage: python -mLemonGraph.server <options> [graphs-dir]', file=fh)
+    print('', file=fh)
+    print('Options:', file=fh)
+    print('  -i <bind-ip>        (127.0.0.1)', file=fh)
+    print('  -p <bind-port>      (8000)', file=fh)
+    print('  -d <poll-delay-ms>  (250)', file=fh)
+    print('  -w <workers>        (#cpu cores)', file=fh)
+    print('  -l <log-level>      (info)', file=fh)
+    print('  -t <timeout>        (3 seconds)', file=fh)
+    print('  -b <buflen>         (1048576)', file=fh)
+    print('  -s                  enable nosync for graph dbs', file=fh)
+    print('  -m                  enable nometasync for graph dbs', file=fh)
+    print('  -r                  rebuild index', file=fh)
+    print('  -h                  print this help and exit', file=fh)
+    if msg is not None:
+        print('', file=fh)
+        print(msg, file=fh)
+    sys.exit(1)
+
+def seed_depth0():
+    while True:
+        txn, entry = yield
+        # first checks stay w/in the entry - last has to instantiate the parent object
+        if entry.is_property and entry.key == 'seed' and entry.value and entry.is_node_property:
+            entry.parent['depth'] = 0
+
+def process_edge(txn, e, cost=1, inf=float('Inf')):
+    # grab latest versions of endpoints
+    src = txn.node(ID=e.srcID)
+    tgt = txn.node(ID=e.tgtID)
+    ds = src.get('depth', inf)
+    dt = tgt.get('depth', inf)
+    if ds + cost < dt:
+        tgt['depth'] = ds + cost
+    elif dt + cost < ds:
+        src['depth'] = dt + cost
+
+def apply_cost(txn, prop):
+    # cost value validity has already been enforced above
+    process_edge(txn, prop.parent, cost=prop.value)
+
+def cascade_depth(txn, prop):
+    # grab latest version of parent node
+    node = txn.node(ID=prop.parentID)
+    mindepth = prop.value + 1
+    for n2 in node.neighbors:
+        try:
+            if n2['depth'] <= mindepth:
+                continue
+        except KeyError:
+            pass
+        n2['depth'] = mindepth
+
+def update_depth_cost():
+    while True:
+        txn, entry = yield
+        if entry.is_edge:
+            # grab newly added edges, and propagate depth
+            process_edge(txn, entry)
+        elif entry.is_property:
+            if entry.key == 'depth':
+                if entry.is_node_property:
+                    cascade_depth(txn, entry)
+            elif entry.key == 'cost':
+                if entry.is_edge_property:
+                    apply_cost(txn, entry)
+
+def main():
+    levels = ('NOTSET', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
+    try:
+        opts, args = getopt(sys.argv[1:], 'i:p:d:l:w:t:b:smrh')
+    except GetoptError as err:
+        usage(msg=str(err))
+    if len(args) > 1:
+        usage()
+
+    default_level = 'info'
+
+    ip = '127.0.0.1'
+    port = 8000
+    logspec = default_level
+    poll = 250
+    nosync = False
+    nometasync = False
+    rebuild = False
+    path = 'graphs'
+    workers = -1
+    timeout = 3
+    buflen = 1048576
+    try:
+        path = args[0]
+    except IndexError:
+        pass
+
+    try:
+        for o, a in opts:
+            if o == '-i':
+                ip = a
+            elif o == '-p':
+                port = sorted((0, int(a), 65536))[1]
+            elif o == '-d':
+                poll = sorted((20, int(a), 10000))[1]
+            elif o == '-s':
+                nosync = True
+            elif o == '-m':
+                nometasync = True
+            elif o == '-r':
+                rebuild = True
+            elif o == '-l':
+                logspec = a
+            elif o == '-t':
+                timeout = sorted((0.010, int(a)))[1]
+            elif o == '-w':
+                workers = int(a)
+            elif o == '-b':
+                buflen = int(a)
+            elif o == '-h':
+                usage(fh=sys.stdout)
+            else:
+                raise RuntimeError(o)
+    except (KeyError, ValueError):
+        usage()
+
+    all_logs = tuple("LemonGraph.%s" % x for x in ('httpd', 'collection', 'server'))
+    log_levels = dict( (k, default_level) for k in all_logs)
+    for token in logspec.split(','):
+        try:
+            target, level = token.split('=', 1)
+            alt = "LemonGraph.%s" % target
+            target = alt if alt in all_logs else target
+            targets = (target,)
+        except ValueError:
+            targets = all_logs
+            level = token
+        if level.upper() not in levels:
+            usage()
+        for target in targets:
+            log_levels[target] = level
+
+    loghandler = LogHandler()
+    for target, level in log_levels.iteritems():
+        logger = logging.getLogger(target)
+        logger.addHandler(loghandler)
+        logger.setLevel(getattr(logging, level.upper()))
+
+    graph_opts=dict(
+        serialize_property_value=Serializer.msgpack(),
+        adapters=Adapters(seed_depth0, update_depth_cost),
+        nosync=nosync, nometasync=nometasync,
+        )
+
+    # initialize the collection up front (which will re-create if missing)
+    # before we turn on the web server
+    col = Collection(path, create=True, rebuild=rebuild, graph_opts=graph_opts)
+    col.close()
+
+    def _syncd():
+        collection = Collection(path, graph_opts=graph_opts, nosync=True, nometasync=False)
+        try:
+            collection.daemon(poll=poll)
+        except Graceful:
+            pass
+        finally:
+            collection.close()
+
+    Server(collection_path=path, graph_opts=graph_opts, extra_procs={'syncd': _syncd}, host=ip, port=port, spawn=workers, timeout=timeout, buflen=buflen)
+
+main()

commit e6a19acc2a37875af96e7d3150027da54b269c33

fixed /view endpoint - broken since e41bd54365a960d69c3f0d6a6a297d89240aa646

LemonGraph/server/__init__.py
@@ -1013,7 +1013,7 @@ class Static(Handler):
         try:
             body = self.cache[resource]
         except KeyError:
-            body = pkg_resources.resource_string(__name__, 'data/%s' % resource)
+            body = pkg_resources.resource_string(__name__, '../data/%s' % resource)
             #self.cache[resource] = body
 
         extension = os.path.splitext(resource)[1]